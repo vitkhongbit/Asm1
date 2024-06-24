d12c4ffdbde830f60b4a0bc15d2e6bcc1b370129
vitkhongbit <123phuongta@gmail.com>
2024-06-24 22:34:23 +0700

    first commit

---
+++ ASM1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+
+                string Choice = EnterSelection();
+
+                if (Choice == "5")
+                {
+                    break;
+                }
+
+                double WaterLastMonth = WaterInput("Enter the amount of water last month: ");
+                double WaterThisMonth = WaterInput("Enter the amount of water this month: ");
+                while (WaterThisMonth < WaterLastMonth)
+                {
+                    Console.WriteLine("The amount of water this month must be greater than the amount last month");
+                    WaterLastMonth = WaterInput("Enter the amount of water last month: ");
+                    WaterThisMonth = WaterInput("Enter the amount of water this month: ");
+
+                }
+                
+                Console.Write("Enter your name:");
+                string name = Console.ReadLine();
+                double WaterConsumption = WaterThisMonth - WaterLastMonth;
+                double Result = Calculate(Choice , WaterLastMonth, WaterThisMonth);
+
+                Console.WriteLine("\n=========== Water bill =============\n");
+                Console.WriteLine("Customer: " + name);
+                Console.WriteLine("Amount of water used last month: " + WaterLastMonth + " (M3)");
+                Console.WriteLine("Amount of water used this month: " + WaterThisMonth + " (M3)");
+                Console.WriteLine("Amount of water consumed: " + WaterConsumption + " (M3)");
+                Console.WriteLine("Total cost: " + Result +" (VND)");
+            }
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("\n====== Cwater bill program ======");
+            Console.WriteLine("1. Household customer\n2. Administrative agency, public services\n3. Production units\n4. Business services\n5. Exit ");
+            Console.WriteLine("Enter customer type: ");
+        }
+
+        static string EnterSelection()
+        {
+            string Choice = Console.ReadLine();
+
+            while (Choice != "1" && Choice != "2" && Choice != "3" && Choice != "4" && Choice != "5")
+            {
+                Console.WriteLine("Re-enter 1 to 5");
+                Choice = Console.ReadLine();
+            }
+            return Choice;
+        }
+
+        static double WaterInput (string message)
+        {
+            Console.Write(message);
+            double WaterAmout ;
+            while (!double.TryParse(Console.ReadLine(), out WaterAmout) || WaterAmout < 0)
+            {
+                Console.WriteLine("Re enter " + message.ToLower());
+            }
+            return WaterAmout;
+        }
+
+        static double Calculate (string Choice, double WaterLastMonth, double WaterThisMonth)
+        {
+
+            double WaterPerPerson = 0;
+            double EnvironmentalTax = 0;
+            double WaterConsumption = WaterThisMonth - WaterLastMonth;
+
+            switch (Choice)
+            {
+
+                case "1":
+                    Console.WriteLine("Customers are households:");
+                    Console.WriteLine("Enter number of users:");
+                    int User;
+                    while (!int.TryParse(Console.ReadLine(), out User) || User < 0)
+                    {
+                        Console.WriteLine("Re enter:");
+                    }
+                    double ConsumptionPerPerson = WaterConsumption / User;
+                    if (ConsumptionPerPerson <= 10)
+                    {
+                        WaterPerPerson = 5973;
+                        EnvironmentalTax = 597.3;
+                    }
+                    else if (ConsumptionPerPerson <= 20)
+                    {
+                        WaterPerPerson = 7052;
+                        EnvironmentalTax = 705.2;
+
+                    }
+                    else if (ConsumptionPerPerson <= 30)
+                    {
+                        WaterPerPerson = 8699;
+                        EnvironmentalTax = 869.9;
+
+                    }
+                    else
+                    {
+                        WaterPerPerson = 15929;
+                        EnvironmentalTax = 1592.9;
+
+                    }
+                    break;
+
+                case "2":
+                    Console.WriteLine("Administrative agency, public services:");
+                    WaterPerPerson = 9955;
+                    EnvironmentalTax = 995.5;
+
+
+                    break;
+                case "3":
+                    Console.WriteLine("Production units:");
+                    WaterPerPerson = 11615;
+                    EnvironmentalTax = 1161.5;
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("Business services:");
+                    WaterPerPerson = 22068;
+                    EnvironmentalTax = 2206.8;
+                    break;
+                default:
+                    Console.WriteLine("Requirement does not exist");
+                    break;
+
+            }
+            double TotalMoney = WaterConsumption * (WaterPerPerson + EnvironmentalTax);
+            double Vat = TotalMoney * 0.1;
+            return TotalMoney + Vat;
+        }
+
+
+    }
+}
---
+++ ASM1/Test.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ASM1
+{
+    internal class Test
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+
+                string luachon = Nhapluachon();
+
+                if (luachon == "5")
+                {
+                    break;
+                }
+
+                double nuocthangtruoc = Nhapluongnuoc("Nhap luong nuoc thang truoc");
+                double nuocthangnay = Nhapluongnuoc("Nhap luong nuoc thang nay");
+
+                
+
+                Console.WriteLine("\n=========== Hoa don tien nuoc =============\n");
+                Console.WriteLine("Nhap ten:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Khach hang: " + name);
+                Console.WriteLine("Luong nuoc dung thang truoc la: " + nuocthangtruoc + " (M3)");
+                Console.WriteLine("Luong nuoc dung thang nay la: " + nuocthangnay + " (M3)");
+                double luongnuoctieuthu = nuocthangnay - nuocthangtruoc;
+                Console.WriteLine("Luong nuoc tieu thu la " + luongnuoctieuthu);
+
+                double ketqua = Tinhtoan(luachon, nuocthangtruoc, nuocthangnay);
+                Console.WriteLine("Tong so tien la: " + ketqua);
+
+            }
+            
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("\n====== Chuong trinh tinh tien nuoc ======");
+            Console.WriteLine("Nhap theo so loai khach hang: ");
+            Console.WriteLine("1. Ho gia dinh\n2. Co quan hanh chinh, dich vu cong cong\n3. Don vi san suat\n4. Dich vu kinh doanh\n5. Thoat ");
+        }
+
+        static string Nhapluachon()
+        {
+            string luachon = Console.ReadLine();
+
+            while (luachon != "1" && luachon != "2" && luachon != "3" && luachon != "4" && luachon != "5")
+            {
+                Console.WriteLine("Nhap lai tu 1 den 5");
+                luachon = Console.ReadLine();
+            }
+            return luachon;
+        }
+
+        static double Nhapluongnuoc(string message)
+        {
+            Console.WriteLine(message);
+            double luongnuoc;
+            while (!double.TryParse(Console.ReadLine() , out luongnuoc) || luongnuoc < 0) 
+            {
+                Console.WriteLine("Nhap lai " + message.ToLower());
+            }
+            return luongnuoc;
+        }
+
+        static double Tinhtoan (string luachon , double nuocthangtruoc , double nuocthangnay)
+        {
+
+            double tiennuocmoinguoi = 0;
+            double thuemoitruong = 0;
+            double luongnuoctieuthu = nuocthangnay - nuocthangtruoc;
+
+            switch (luachon)
+            {
+
+                case "1":
+                    Console.WriteLine("Khach hang la ho gia dinh:");
+                    Console.WriteLine("Nhap so nguoi su dung:");
+                    int nguoisudung;
+                    while (!int.TryParse(Console.ReadLine(), out nguoisudung) || nguoisudung < 0)
+                    {
+                        Console.WriteLine("Nhap lai:");
+                    }
+                    double luongtieuthumoinguoi = luongnuoctieuthu / nguoisudung;
+                    if (luongtieuthumoinguoi <= 10)
+                    {
+                        tiennuocmoinguoi = 5973;
+                        thuemoitruong = 597.3;
+                    }
+                    else if (luongtieuthumoinguoi <= 20)
+                    {
+                        tiennuocmoinguoi = 7052;
+                        thuemoitruong = 705.2;
+
+                    }
+                    else if (luongtieuthumoinguoi <= 30)
+                    {
+                        tiennuocmoinguoi = 8699;
+                        thuemoitruong = 869.9;
+
+                    }
+                    else
+                    {
+                        tiennuocmoinguoi = 15929;
+                        thuemoitruong = 1592.9;
+
+                    }
+                    break;
+
+                case "2":
+                    Console.WriteLine("Co quan hanh chinh, dich vu:");
+                    tiennuocmoinguoi = 9955;
+                    thuemoitruong = 995.5;
+
+
+                    break;
+                case "3":
+                    Console.WriteLine("Don vi san suat:");
+                    tiennuocmoinguoi = 11615;
+                    thuemoitruong = 1161.5;
+
+                    break;
+
+                case "4":
+                    Console.WriteLine("Dich vu kinh doanh:");
+                    tiennuocmoinguoi = 22068;
+                    thuemoitruong = 2206.8;
+                    break;
+                default:
+                    Console.WriteLine("khong ton tai yeu cau");
+                    break;
+
+            }
+            double tongtien = luongnuoctieuthu * (tiennuocmoinguoi + thuemoitruong);
+            double vat = tongtien * 0.1;
+            return tongtien + vat;
+        }
+        
+
+
+    }
+}
